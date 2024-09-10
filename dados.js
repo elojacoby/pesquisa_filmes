b01f5604c0576d7d9d21d3b098f11b5a4d36654e
elojacoby <163744000+elojacoby@users.noreply.github.com>
2024-09-10 14:50:09 -0300

    Pesquise seu filme

---
+++ dados.js
@@ -0,0 +1,33 @@
+let dados =[ 
+    { titulo : "A cinco passos de você",
+    descricao:"A Cinco Passos de Você é um filme que nos convida a refletir sobre a vida, o amor e a força do espírito humano. A trama gira em torno de Stella e Will, dois adolescentes que lutam contra a fibrose cística. Confrontados pelas rigorosas regras do hospital que os impedem de se aproximar fisicamente, eles encontram uma conexão profunda e apaixonada. O filme aborda temas como a esperança, a amizade e a importância de viver cada momento intensamente, mesmo diante de desafios imensos.",
+    link: "https:/wwwtecmundocombrminha-serie280042-cinco-passos-voce-filme-baseado-historia-real.htm",
+    tags:"amor, fibrose, amizade"
+    },
+
+    {
+         titulo : "Presságio",
+         descricao:"Presságio é um filme que nos leva a uma emocionante jornada através do tempo e do espaço. A trama gira em torno de um grupo de cientistas que descobrem um conjunto de símbolos antigos que parecem prever eventos catastróficos do futuro. Ao decifrar esses símbolos, eles se veem em uma corrida contra o tempo para tentar alterar o curso da história e evitar uma catástrofe global.",
+        link: "https://rollingstone.com.br/noticia/conheca-pressagio-filme-de-suspense-e-assassinato-que-chegou-ao-topo-da-netflix-no-brasil/",
+         tags:""
+    },
+        { 
+            titulo : "Before We Go",
+            descricao :"Antes de Partir (Before We Go) é um filme que nos transporta para uma noite mágica e cheia de emoções em Nova York. A trama gira em torno de um encontro casual entre Nick, um músico talentoso, e Brooke,uma jovem com segredos a revelar. Ao longo da noite, eles exploram a cidade, conversam sobre a vida e se conectam de forma profunda. O filme explora temas como amor, sonhos, arrependimentos e a importância de aproveitar cada momento",
+            link: "https://pseudocritico.com/2020/07/27/critica-antes-do-adeus/",
+             tags:""
+
+        },
+        {
+            titulo: "John Wick",
+            descricao: "John Wick é um ex-assassino que leva uma vida pacata até que um grupo de criminosos invade sua casa e mata seu cachorro, o último presente de sua falecida esposa. Enfurecido, John retorna à vida criminosa para se vingar, desencadeando uma guerra sangrenta contra as maiores organizações criminosas do mundo.",
+            link: "https://www.imdb.com/title/tt2911666/",
+            tags:""
+
+        },
+        {titulo: "Interstellar",
+            descricao: "Em um futuro próximo, a Terra está se tornando inabitável. Um grupo de astronautas embarca em uma missão para encontrar um novo planeta habitável, explorando buracos de minhoca e viajando pelos confins do universo. A jornada os levará a questionar a natureza do tempo, o amor e a própria existência da humanidade.",
+            link: "https://www.imdb.com/title/tt0816692/",  
+             tags:""
+        },
+];
